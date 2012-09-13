61fd728821756d12a0b67842e9fbe8f40cdab03a
Yong Lin <yonglin88@gmail.com>
2012-09-12 22:15:42 -0700

    first commit

---
+++ MuseMe/MuseMeApp/MuseMeApp/CommentCell.h
@@ -0,0 +1,13 @@
+//
+//  CommentCell.h
+//  MuseMe
+//
+//  Created by Yong Lin on 9/12/12.
+//  Copyright (c) 2012 Stanford University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentCell : UITableViewCell
+
+@end
